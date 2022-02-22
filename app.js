8ad92930c83ccd6e82104aae5c50ecc7f4d62b69
dxn7335 <dxn7335@rit.edu>
2022-02-22 15:51:54 -0500

    Create Mock Data feature for dev

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const indexRouter = require('./routes/index');
+const propertyRouter = require('./routes/property/');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 /**
  * Route Handling
@@ -10,6 +15,7 @@ const app = express();
  * All api routes here
  */
 app.use('/', indexRouter);
+app.use('/property', propertyRouter);
 
 
 module.exports = app;
--- config.js
+++ config.js
@@ -1,34 +1,62 @@
 const path = require('path');
 const createError = require('http-errors');
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
+require('dotenv').config();
 
 const applyConfig = (app) => {
-    // view engine setup
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-    app.use(cors());
+  app.use(cors());
 
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-    // catch 404 and forward to error handler
-    app.use(function(req, res, next) {
-        next(createError(404));
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    return next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  // Mocking data for dev purposes
+  if (process.env.ENVIRONMENT === 'dev') {
+    // Intercept outbound calls and use the path to return correct mock data
+    axios.interceptors.request.use(req => {
+      const url = new URL(req.url);
+      const routepath = url.pathname.split('/v2/')[1];
+      return {
+          ...req,
+          url: `/mocks/${routepath}/`,
+      };
     });
-    
-    // error handler
-    app.use(function(err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-    
-        // render the error page
-        res.status(err.status || 500);
-        res.render('error');
+
+    axios.interceptors.response.use((resp) => {
+      return resp;
+    }, (error) => {
+      const mockData = require(`.${error.config.url}/mockdata.json`);
+      return Promise.resolve({
+        data: mockData,
+        status: 200,
+        statusText: 'OK',
+        headers: error.config.headers,
+        config: error.config,
+      });
     });
+  }
 }
 
 module.exports = applyConfig;
