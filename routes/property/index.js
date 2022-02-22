aa8fb0ee39105bf2bf2051e2a1e0e68479669147
dxn7335 <dxn7335@rit.edu>
2022-02-22 17:43:04 -0500

    Remove duplicated import

--- routes/property/index.js
+++ routes/property/index.js
@@ -5,7 +5,6 @@ const router = express.Router();
 // sub-route controllers
 const detailsController = require('./details/details');
 const detailSepticController = require('./details/details-hasSeptic');
-const detailsSepticController = require('./details/details-hasSeptic');
 
 /* Sub Routes */
 router.get('/details', detailsController);
